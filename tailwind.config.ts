7a3467af481f7979b026c93ddf4cafb1f1b84787
rkmiller131 <rkmiller131@gmail.com>
2024-10-31 16:56:35 -0700

    Added fonts and favicon

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,6 +12,10 @@ const config: Config = {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+      fontFamily: {
+        primary: ["var(--font-jaldi)", "serif"],
+        secondary: ["var(--font-inter)", "sans-serif"],
+      },
     },
   },
   plugins: [],
