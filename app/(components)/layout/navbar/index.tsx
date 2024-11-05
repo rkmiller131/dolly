202c6dd46e324509420893e40073dc975d1c6f63
rkmiller131 <rkmiller131@gmail.com>
2024-11-05 13:42:40 -0800

    active nav link for different "create" btn color

--- app/(components)/layout/navbar/index.tsx
+++ app/(components)/layout/navbar/index.tsx
@@ -1,7 +1,11 @@
+'use client'
 import Link from "next/link";
 import Button from "../../ui/buttons/Button";
+import { usePathname } from "next/navigation";
 
 export default function Navbar() {
+  const pathname = usePathname();
+  const color = pathname === "/create" ? "complement" : "callout";
   return (
     <nav className="w-full bg-secondary drop-shadow-md">
       <div className="container mx-auto py-4 flex justify-between">
@@ -11,7 +15,7 @@ export default function Navbar() {
         <Button
           text="Create"
           interaction={{ type: "navigate", href: "/create" }}
-          color="callout"
+          color={color}
         />
       </div>
     </nav>
